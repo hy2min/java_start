fd60a4bf5d4ce4db75cbcfcc799aca667e7f2999
hy2min <hy1x1mn@gmail.com>
2025-01-04 23:41:54 +0900

    Session9: ArrayRef2

--- src/array/ArrayRef1.java
+++ src/array/ArrayRef1.java
@@ -12,8 +12,10 @@ public class ArrayRef1 {
         students[3] = 60;
         students[4] = 50;
 
-        for (int i = 0; i <= 4; i++) {
-            System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
-        }
+        System.out.println("학생1 점수: " + students[0]);
+        System.out.println("학생2 점수: " + students[1]);
+        System.out.println("학생3 점수: " + students[2]);
+        System.out.println("학생4 점수: " + students[3]);
+        System.out.println("학생5 점수: " + students[4]);
     }
 }

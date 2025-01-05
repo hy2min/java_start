3f7007f54a6ed81d4429a9855aa3542b364fbe6d
hy2min <hy1x1mn@gmail.com>
2025-01-05 21:36:21 +0900

    Session9: ArrayRef

--- src/array/ArrayRef2.java
+++ src/array/ArrayRef2.java
@@ -3,9 +3,16 @@ package array;
 public class ArrayRef2 {
     public static void main(String[] args) {
         int[] students;
-        students = new int[]{90, 80, 70, 60, 50}; //배열 생성과 초기화
+        students = new int[5]; //배열 생성과 초기화
 
-        for (int i = 0; i < students.length; i++) {
+        students[0] = 90;
+        students[1] = 80;
+        students[2] = 70;
+        students[3] = 60;
+        students[4] = 50;
+
+
+        for (int i = 0; i < 5; i++) {
             System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
         }
     }

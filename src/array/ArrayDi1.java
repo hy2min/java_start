cdc6ee2b0b9a773c9a08b6f239ea0bb5cb318876
hy2min <hy1x1mn@gmail.com>
2025-01-05 21:38:09 +0900

    Session9: ArrayDi1

--- src/array/ArrayDi1.java
+++ src/array/ArrayDi1.java
@@ -1,6 +1,6 @@
 package array;
 
-public class ArrayDi0 {
+public class ArrayDi1 {
     public static void main(String[] args) {
         int[][] arr = new int[2][3]; //행2, 열3
 
@@ -11,16 +11,12 @@ public class ArrayDi0 {
         arr[1][1] = 5; //1행, 1열
         arr[1][2] = 6; //1행, 2열
 
-        //0행 출력
-        System.out.print(arr[0][0]+ " "); //0열 촐력
-        System.out.print(arr[0][1]+ " "); //1열 출력
-        System.out.print(arr[0][2]+ " "); //2열 출력
-        System.out.println(); //한 행이 끝나면 라인을 변경하다.
+        for (int row = 0; row < 2; row++) {
+            System.out.print(arr[row][0]+ " "); //0열 촐력
+            System.out.print(arr[row][1]+ " "); //1열 출력
+            System.out.print(arr[row][2]+ " "); //2열 출력
+            System.out.println(); //한 행이 끝나면 라인을 변경하다.
 
-        //1행 출력
-        System.out.print(arr[1][0]+ " "); //0열 촐력
-        System.out.print(arr[1][1]+ " "); //1열 출력
-        System.out.print(arr[1][2]+ " "); //2열 출력
-        System.out.println(); //한 행이 끝나면 라인을 변경하다.
+        }
     }
 }
